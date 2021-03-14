11cbb94a5a806452b0f082d83af5b675e8a840f1
kaufmjoh <kaufmjoh@oregonstate.edu>
2021-03-13 18:44:43 -0800

    Updated test file to eliminate linting errors

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,10 @@
 import calculator
 
+
 class TestCalculator:
-	
-	def test_addition(self):
-		assert(calculator.addition(2,3) == 5)
-	
-	def test_subtraction(self):
-		assert(calculator.subtraction(2,3) == -1)
+
+    def test_addition(self):
+        assert(calculator.addition(2, 3) == 5)
+
+    def test_subtraction(self):
+        assert(calculator.subtraction(2, 3) == -1)
